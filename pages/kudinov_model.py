5dfdbfd3102bc009486e429dbb42d367c09e1ec2
kudinov <s.kudinov@list.ru>
2024-07-26 13:53:18 +0300

    update

--- pages/kudinov_model.py
+++ pages/kudinov_model.py
@@ -1,5 +1,6 @@
 import torch
 from models.model_kudinov import Model
+from models.kudinov_preprocessing import preprocess
 import streamlit as st
 from torchvision import io
 from PIL import Image
@@ -13,8 +14,19 @@ def round_by_threshold(value):
     return 1 if value >= TRESHOLD else 0
 
 
-model = Model()
-model.load_state_dict(torch.load('models/model_kudinov.pt', map_location=torch.device('cpu')))
+@st.cache_resource()
+def load_model():
+    model = Model()
+    model.load_state_dict(torch.load('models/model_kudinov.pt', map_location=torch.device('cpu')))
+    return model
+
+model = load_model()
+
+def predict(img):
+    img = preprocess(img)
+    pred = model(img)
+    model.eval()
+    return pred
 
 st.title('Модель по определению опухолей на коже')
 st.caption('От Серёжи')
@@ -22,20 +34,10 @@ st.divider()
 
 uploaded_image = st.file_uploader("Кидай свою опухоль")
 
-if uploaded_image is not None:
+if uploaded_image:
+    img = Image.open(uploaded_image)
+    pred_prob = predict(img).item()
+    pred_class = round_by_threshold(pred_prob)
 
-    img = Image.open(uploaded_image).convert('RGB')
-
-    transform = T.Compose([
-        T.Resize((224, 224)),
-        T.ToTensor()
-    ])
-    resize = T.Resize((224, 224))
-    img = transform(img)
-    img = resize(img / 255)
-    with torch.inference_mode():
-        pred_prob = model(img.unsqueeze(0)).item()
-        pred_class = round_by_threshold(pred_prob)
-
-    st.write(f'Опухоль {idx2class[pred_class]}, p: {pred_prob:.2f}')
+    st.write(f'Опухоль {idx2class[pred_class]}, p: {pred_prob}')
     st.image(uploaded_image, caption='Загруженное изображение', use_column_width=True)
