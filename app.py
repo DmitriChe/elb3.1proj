96505de50001eef3c38fba8081a93542d53dd099
DmitriChe <juneority@mail.ru>
2024-07-27 17:36:17 +0300

    Add files via upload
    Добавлена ссылка на страницу с моделью resnet dmche

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ st.caption('От Димы, Толубая и Серёжи')
 st.divider()
 
 col1, col2, col3 = st.columns(3)
+with col1:
+    st.page_link('pages/dmche_model.py', label='Модель ДимыЧе')
+    st.page_link('pages/dmche_model_info.py', label='more', icon='👀')
 
 with col3:
     st.page_link('pages/kudinov_model.py', label='Модель Серёжи', icon='👾')
